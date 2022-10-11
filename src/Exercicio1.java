8c8ff5f2522c5f65fea01b2761b724b2cf30f848
Hejociara <hejociarav@gmail.com>
2022-10-11 20:44:15 -0300

    "Feature"

---
+++ src/Exercicio1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        entrada.close();
+        System.out.println();
+    }
+}
