836f8f6ca4b0e0cae6b8ba7a0bae8e20f4039401
Hejociara <hejociarav@gmail.com>
2022-10-11 22:03:16 -0300

    "Feature"

--- src/Exercicio1.java
+++ src/Exercicio1.java
@@ -2,18 +2,25 @@ import java.util.Scanner;
 
 /**
  * @author: Hejociara
- * Criar um programa que receba um número e verifique se ele está entre 0 e 10 e é par;
  */
 public class Exercicio1 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
+        System.out.println("Digite um número: ");
         int num = entrada.nextInt();
 
-        if (num > 0 && num <= 10)
-            System.out.println("O número é par" );
-
-        entrada.close();
-
+        if (num >= 0 && num <= 10) {
+            if (num % 2 == 0) {
+                System.out.println("O número " + num + " está entre 0 e 10 e é par.");
+            } else {
+                System.out.println("O número " + num + "está entre 0 e 10 mas não é par.");
+            }
+        }
+        else{
+                System.out.println("O número" + num + " não está entre 0 e 10.");
+            }
+            entrada.close();
+        }
     }
-}
+
