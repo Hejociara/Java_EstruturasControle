102eb824a15587da657a207e14a5713ceb81c10b
Hejociara <hejociarav@gmail.com>
2022-10-11 21:05:23 -0300

    "Feature"

--- src/Exercicio1.java
+++ src/Exercicio1.java
@@ -1,10 +1,19 @@
 import java.util.Scanner;
 
+/**
+ * @author: Hejociara
+ * Criar um programa que receba um número e verifique se ele está entre 0 e 10 e é par;
+ */
 public class Exercicio1 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
+        int num = entrada.nextInt();
+
+        if (num > 0 && num <= 10)
+            System.out.println("O número é par" );
+
         entrada.close();
-        System.out.println();
+
     }
 }
