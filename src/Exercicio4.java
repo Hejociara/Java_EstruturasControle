0d726e07257a23b9d111ae68fecbc577c27fc832
Hejociara <hejociarav@gmail.com>
2022-10-13 22:45:42 -0300

    "Feature"

--- src/Exercicio4.java
+++ src/Exercicio4.java
@@ -6,16 +6,24 @@ import java.util.Scanner;
 //Criar um programa que receba um número e diga se ele é um número primo.
 public class Exercicio4 {
     public static void main(String[] args) {
+
+        int contadorDivisores = 0;
         Scanner entrada = new Scanner(System.in);
 
-        double num = entrada.nextDouble();
+        System.out.println("Digite um número para verificar se é primo:");
+        int num = entrada.nextInt();
 
-        if(num)
+        for(int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                contadorDivisores++;
+            }
+        }
+        if(contadorDivisores == 0) {
             System.out.println(num + " é número primo");
-            else
-        System.out.println(num + " não é primo");
-
-
+        }
+        else{
+                System.out.println(num + " não é primo");
+            }
         entrada.close();
     }
 }
