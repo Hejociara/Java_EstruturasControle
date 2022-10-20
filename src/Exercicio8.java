67955f8e90b8b09346d38cc149b71c1d7ad88bf9
Hejociara <hejociarav@gmail.com>
2022-10-20 18:32:02 -0300

    "Feature"

---
+++ src/Exercicio8.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+// Crie um programa que recebe 10 valores e
+// ao final imprima o maior número.
+public class Exercicio8 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int valor;
+
+        entrada.close();
+    }
+}
