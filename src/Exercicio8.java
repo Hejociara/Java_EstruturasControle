012454587f01a72b13eaf0ccd6594396d1a402dc
Hejociara <hejociarav@gmail.com>
2022-10-21 21:57:29 -0300

    "Feature"

--- src/Exercicio8.java
+++ src/Exercicio8.java
@@ -1,11 +1,18 @@
 import java.util.Scanner;
 
-// Crie um programa que recebe 10 valores e
-// ao final imprima o maior número.
+//Criar um programa que receba uma palavra e
+// imprime no console letra por letra.
 public class Exercicio8 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int valor;
+        System.out.println("Digite a palavra: ");
+        String palavra = entrada.nextLine();
+
+        char letras[] = palavra.toCharArray();
+
+        for (int i = 0; i < letras.length; i++) {
+            System.out.println(letras[i]);
+        }
 
         entrada.close();
     }
